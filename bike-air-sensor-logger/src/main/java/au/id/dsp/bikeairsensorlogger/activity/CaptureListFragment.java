78b5172fe3623c465ca0fd7b0e3756680494cdae
Damien <damien@sail>
2015-10-12 20:17:36 +1030

    Format the start time of each capture

--- bike-air-sensor-logger/src/main/java/au/id/dsp/bikeairsensorlogger/activity/CaptureListFragment.java
+++ bike-air-sensor-logger/src/main/java/au/id/dsp/bikeairsensorlogger/activity/CaptureListFragment.java
@@ -15,14 +15,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import au.id.dsp.bikeairsensorlogger.R;
 import au.id.dsp.bikeairsensorlogger.bluetooth.CaptureProvider;
 import au.id.dsp.bikeairsensorlogger.bluetooth.LogDatabase;
 
 public class CaptureListFragment extends ListFragment {
+
+    private static final DateFormat dateFormat;
+    static {
+        DateFormat f = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+        if (f instanceof SimpleDateFormat)
+            // Add the time zone
+            f = new SimpleDateFormat(((SimpleDateFormat) f).toPattern() + " z");
+        else
+            f = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        dateFormat = f;
+    }
 
     private LogDatabase logDatabase;
     private SimpleCursorAdapter adapter;
@@ -68,18 +83,24 @@ public class CaptureListFragment extends ListFragment {
     public void onAttach(Activity activity) {
         logDatabase = new LogDatabase(activity);
         super.onAttach(activity);
+
+        final Cursor cursor = logDatabase.getCaptures().createCursor();
         adapter = new SimpleCursorAdapter(activity,
                 R.layout.fragment_capture_list_row,
-                logDatabase.getCaptures().createCursor(),
+                cursor,
                 new String[] { "NAME", "START" },
                 new int[] { R.id.nameView, R.id.startTimeView },
-                0) {
+                0);
+        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
             @Override
-            public void bindView(View view, Context context, Cursor cursor) {
-                super.bindView(view, context, cursor);
-                // TODO: Count and status
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (cursor.getColumnIndexOrThrow("START") == columnIndex)
+                    ((TextView) view).setText(dateFormat.format(new Date(cursor.getLong(columnIndex))));
+                else
+                    return false;
+                return true;
             }
-        };
+        });
         setListAdapter(adapter);
     }
 
